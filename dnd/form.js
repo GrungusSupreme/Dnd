7a3196f4e8edf39cfd4abdf7fc6549f69ce26bbd
Folcra <54424560+Folcra@users.noreply.github.com>
2022-12-25 08:46:01 -0600

    added a new script

---
+++ dnd/form.js
@@ -0,0 +1,6 @@
+function saveinfo(){
+    var playerName = document.getElementById("nameInput").value;
+    var playerRace = document.getElementById("raceInput").value;
+    var playerClass = document.getElementById("classInput").value;
+    console.log(playerName+playerRace+playerClass)
+}
